259aa7fab1fd2aa644e65234d55d24c9caf8c813
Lucas <lucasbalduino2@gmail.com>
2024-11-17 15:58:40 -0300

    first commit

---
+++ Models/Professor.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    public class Professor : Pessoa
+    {
+        public decimal Salario { get; set; }
+
+        public override void Apresentar()
+        {
+            Console.WriteLine($"olá, me chamo {Nome}, tenho {Idade} e meu salário é de {Salario}");
+        }
+    }
+}
